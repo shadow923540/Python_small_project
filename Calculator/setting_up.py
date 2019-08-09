9d18593c2c5a43f6492d533b89311c99973213e6
shadow923540 <Naruto#sasuke123!>
2019-08-09 13:14:59 +0200

    Adding user input

--- Calculator/setting_up.py
+++ Calculator/setting_up.py
@@ -17,7 +17,33 @@ class RefiEval():
         Label(window, text="Payment:").grid(row=5, column=1, sticky=W)
         Label(window, text="Total Payements:").grid(row=6, column=1, sticky=W)
 
+        #variables to store inputs
+        self.pv = StringVar()
+        self.interest_rate = StringVar()
+        self.term = StringVar()
+
+        #Variables payment output
+        self.pmt = StringVar()
+        self.total = StringVar()
+
+        #declare entry widget
+        Entry(window, textvariable = self.pv,
+              justify=RIGHT).grid(row=1, column =2, padx = (0,5))
+        Entry(window, textvariable = self.interest_rate,
+              justify=RIGHT).grid(row=2,column =2, padx = (0,5))
+        Entry(window, textvariable = self.term,
+              justify=RIGHT).grid(row=3, column =2, padx = (0,5))
+
+        Label(window, textvariable=self.pmt,
+              font = "Helvetica 12 bold",
+              justify=RIGHT).grid(row=5, column=2, sticky = E)
+
+        Label(window, textvariable=self.pmt,
+              font = "Helvetica 12 bold",
+              justify=RIGHT).grid(row=6, column=2, sticky = E)
+
         window.mainloop()
+
 
 
 RefiEval()
