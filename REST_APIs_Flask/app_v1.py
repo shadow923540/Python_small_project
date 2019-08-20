9890b939712692f1569e9131080d676b5d090952
shadow923540 <Naruto#sasuke123!>
2019-08-20 13:59:24 +0200

    Added first  basic flask app

--- REST_APIs_Flask/app_v1.py
+++ REST_APIs_Flask/app_v1.py
@@ -3,11 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#What request do we want?
-
-@app.route('/') # 'http://www.google.com/'
+@app.route('/')
 def home():
     return "Hello, world!"
-
 
 app.run(port=5000)
