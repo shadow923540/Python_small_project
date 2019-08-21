1c894c9b52014d9f1e53c715203cbdf88e2bb1bd
shadow923540 <Naruto#sasuke123!>
2019-08-21 12:30:14 +0200

    implementing another endpoints

--- REST_APIs_Flask/app_v2.py
+++ REST_APIs_Flask/app_v2.py
@@ -42,12 +42,24 @@ def get_stores():
 #POST /store/<string:name>/item {name, price:}
 @app.route('/stpre/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
 
 #GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
 
 
 app.run(port=5000)
