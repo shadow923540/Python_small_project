cdf4ceb505a468e6b283595eccf4a58e85db96f3
shadow923540 <Naruto#sasuke123!>
2019-08-23 14:14:14 +0200

    declare status message (404,201)

--- REST_APIs_Flask/rest_app.py
+++ REST_APIs_Flask/rest_app.py
@@ -4,19 +4,20 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api= Api(app)
 
-items []
+items= []
 
 
 class Item(Resource):
     def get(self, name):
         for item in items:
             if item['name'] == name:
-                return item
+                return item #We don't have to use jsonify because flask_restful can handle dic
+        return {'item': None}, 404
 
     def post(self, name):
         item = {'name': name, 'price': 12.00}
         items.append(item)
-        return item #Let know the app that we add an item
+        return item, 201 #Let know the app that we add an item
 
 
 api.add_resource(Item, '/item/<string:name>')
