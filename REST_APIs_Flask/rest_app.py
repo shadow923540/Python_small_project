49feb1cb2a8b60bab4b601655bd3765bb7b5dcdf
shadow923540 <Naruto#sasuke123!>
2019-08-23 13:37:37 +0200

    added get item method

--- REST_APIs_Flask/rest_app.py
+++ REST_APIs_Flask/rest_app.py
@@ -9,9 +9,21 @@ items []
 
 class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item #Let know the app that we add an item
+
 
 api.add_resource(Item, '/item/<string:name>')
+
+
+
+
 
 
 app.run(port=5000)
