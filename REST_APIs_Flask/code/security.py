c7a03a63918db69fe0b456fc464ff3a27d9a6c28
zuczkows <pz921@wp.pl>
2019-08-26 10:52:06 +0200

    added new user class which create users and save to db changed security.py now it use find_by_id method from user.py

--- REST_APIs_Flask/code/security.py
+++ REST_APIs_Flask/code/security.py
@@ -11,10 +11,10 @@ userid_mapping = {u.id: u for u in users}
 
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None )
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
