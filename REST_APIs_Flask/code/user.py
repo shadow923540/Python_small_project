1b5bd4b2b2e600d3ddc191d91ec2986fc8d981a0
zuczkows <pz921@wp.pl>
2019-08-26 14:00:02 +0200

    added class for userregister

--- REST_APIs_Flask/code/user.py
+++ REST_APIs_Flask/code/user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 
 
@@ -42,3 +43,27 @@ class User:
 
         connection.close()
         return user
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="this field cannot be left blank!")
+    parser.add_argument('password',
+                        type=str,
+                        reqiored=True,
+                        help="This field cannot be left blank!"
+                        )
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO users VALUES (NULL, ?,?)"
+        cursor.execute(query, (data['username'], data['password'],))
+
+        connection.commit()
+        connection.close()
+
+        return("message": "User created successfully."), 201
