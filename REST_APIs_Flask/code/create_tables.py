278bbc1245e33c4001a0970c92207e1122bede6a
zuczkows <pz921@wp.pl>
2019-08-26 15:37:43 +0200

    added new table with items

--- REST_APIs_Flask/code/create_tables.py
+++ REST_APIs_Flask/code/create_tables.py
@@ -8,6 +8,11 @@ cursor = connection.cursor()
 create_teable = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_teable)
 
+create_teable = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute(create_teable)
+
+cursor.execute("INSERT INTO items VALUES ('test', 10.23)")
+
 connection.commit()
 connection.close()
 
