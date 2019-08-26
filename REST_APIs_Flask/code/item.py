5c288a40b01002232b8cab4e905902c9a726db8d
zuczkows <pz921@wp.pl>
2019-08-26 18:52:18 +0200

    refactor post delete endpoint to work with sqlite3

--- REST_APIs_Flask/code/item.py
+++ REST_APIs_Flask/code/item.py
@@ -12,6 +12,13 @@ class Item(Resource):
                         )
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -19,24 +26,33 @@ class Item(Resource):
         result = cursor.execute(query, (name,))
         row = result.fetchone()
         connection.close()
-
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
-        return {'message': 'Item not found'}, 404
 
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name,items), None) is not None:
+        if self.find_by_name(name):
             return {'message': 'An item exist'}, 400
+
         data = Item.parser.parse_args()
-        # data = request.get_json()
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?,?)"
+        cursor.execute(query, (item['name'], item['price']))
+        connection.commit()
+        connection.close()
         return item, 201 #Let know the app that we add an item
 
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+        connection.commit()
+        connection.close()
         return {'message': 'Item deleted'}
 
     def put(self, name):
@@ -52,4 +68,9 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        return {'items': items}
+        connection = sqlite3.connect()
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        return result
