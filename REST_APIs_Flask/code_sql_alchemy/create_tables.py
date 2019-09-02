c302b77b30181fd2237d061b70c7c9b81ea86396
shadow923540 <Naruto#sasuke123!>
2019-09-02 10:50:51 +0200

    Refactor

--- REST_APIs_Flask/code_sql_alchemy/create_tables.py
+++ REST_APIs_Flask/code_sql_alchemy/create_tables.py
@@ -8,7 +8,7 @@ cursor = connection.cursor()
 create_teable = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_teable)
 
-create_teable = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+create_teable = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)"
 cursor.execute(create_teable)
 
 
--- REST_APIs_Flask/code_sql_alchemy/flask_restful_app.py
+++ REST_APIs_Flask/code_sql_alchemy/flask_restful_app.py
@@ -7,7 +7,8 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
-app.config('SQLALCHEMY_TRACK_MODIFICATIONS') = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #Root folder
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api= Api(app)
 
